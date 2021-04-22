1058ac821786cca302d9b82ae13966d3efbc69a7
Felix Green <work@felix.green>
2021-04-22 14:53:49 -0400

    cleanup

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,15 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
-import { chromeExtension } from "vite-plugin-chrome-extension";
+import { defineConfig } from 'vite'
+import { chromeExtension } from 'vite-plugin-chrome-extension'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            "@": resolve(__dirname, "src"),
-        },
+  plugins: [chromeExtension()],
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
+  build: {
+    rollupOptions: {
+      input: 'src/manifest.json',
     },
-    build: {
-        rollupOptions: {
-            input: "src/manifest.json"
-        }
-    },
-    plugins: [
-        chromeExtension()
-    ],
+  },
 })

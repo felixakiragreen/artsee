39b71342fb9b3e3b07c5d7af01f07fdb3abbb74c
Felix Green <work@felix.green>
2021-04-24 18:28:45 -0400

    render the things

--- src/newtab/App.svelte
+++ src/newtab/App.svelte
@@ -1,66 +1,21 @@
 <script lang="ts">
-  import logo from '../assets/svelte.png'
+  import Settings from './Settings.svelte'
+  import Frame from './Frame.svelte'
+
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
 
+  <Settings />
+  <Frame />
 
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
 </main>
 
 <style style lang="postcss">
-  /* :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  } */
-
   main {
     @apply bg-grey-100 dark:bg-grey-900;
-    @apply w-screen h-screen;
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
+    @apply w-screen h-screen max-h-screen max-w-full;
+    @apply flex items-center justify-center;    
   }
 
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    @apply text-red-800;
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
 </style>

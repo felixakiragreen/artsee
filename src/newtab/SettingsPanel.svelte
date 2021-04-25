28e96d2bd3c725e2f9c486110144e0981b15108a
Felix Green <work@felix.green>
2021-04-25 18:03:31 -0400

    polish up the styling of the settings panel

--- src/newtab/SettingsPanel.svelte
+++ src/newtab/SettingsPanel.svelte
@@ -1,28 +1,8 @@
 <script lang="ts">
-  import { onMount, tick } from 'svelte'
-
-  import { model } from './store'
+  import { model, logStorage, deleteStorage } from '../model'
   
   const sync = async () => {
-    // console.log("b4", $model)
-    
     await model.fetchAll()
-    await tick()
-    // wallet.update(m => "haha")
-
-    // console.log("a5", $model)
-  }
-
-  const logStorage = async () => {
-    chrome.storage.sync.get(null, (data) => {
-      console.log('Settings.logStorage() →', { data })
-    })
-  }
-
-  const delStorage = async () => {
-    chrome.storage.sync.clear(() => {
-      console.log('Settings.delStorage()')
-    })
   }
 
   $: syncedDate = $model.status?.finished ? new Date($model.status?.finished).toISOString() : "n/a"
@@ -47,7 +27,7 @@
     <div>
       <button on:click={sync}>sync</button>
       <button on:click={logStorage}>log storage</button>
-      <button on:click={delStorage}>del storage</button>
+      <button on:click={deleteStorage}>del storage</button>
     </div>
   </dd>
 
@@ -76,27 +56,31 @@
     
     @apply text-base;
     @apply text-grey-800 dark:text-gray-200;
+    @apply focus:text-green-800 focus:dark:text-green-200;
     
     @apply px-4 pt-2 pb-1;
     @apply rounded;
     @apply w-[450px];
 
-    @apply focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-green-300 dark:focus:ring-green-700;
+    @apply ring-green-300 dark:ring-green-700;
+    @apply hover:ring-1;
+    @apply focus:outline-none focus:ring-2 focus:ring-offset-0;
   }
 
   button {
     @apply bg-grey-300 dark:bg-grey-700 hover:bg-grey-100 hover:dark:bg-grey-900;
 
-    @apply border border-gray-500;
+    @apply border border-gray-500 hover:border-green-500;
 
     @apply text-sm font-medium;
     @apply text-grey-800 dark:text-gray-200 text-base;
+    @apply hover:text-green-800 hover:dark:text-green-200;
     
-    @apply px-2 py-1;
+    @apply px-4 py-2;
     @apply shadow-sm;
     @apply rounded-md;
 
-    @apply focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-green-700 dark:focus:ring-green-700;
+    @apply focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-green-700 focus:dark:ring-green-700;
   }
 
   dl {

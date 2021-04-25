e47d4b6cf73c4f2349bbc66ef184a60c5093412d
Felix Green <work@felix.green>
2021-04-25 18:03:39 -0400

    polish styles

--- src/newtab/Settings.svelte
+++ src/newtab/Settings.svelte
@@ -58,7 +58,7 @@
     @apply cursor-pointer;
     @apply absolute;
 
-    @apply text-grey-900 dark:text-gray-100 hover:text-grey-700 hover:dark:text-gray-300;
+    @apply text-grey-900 dark:text-gray-100 hover:text-green-700 hover:dark:text-green-300;
     @apply hover:bg-grey-300 hover:dark:bg-gray-700;
     @apply bg-opacity-25 hover:bg-opacity-50; 
 
@@ -68,53 +68,6 @@
     &.close {
       @apply top-4 right-4;
     }
-  }
-
-  h2 {
-    @apply text-2xl;
-  }
-
-  input[type="text"] {
-    @apply bg-grey-300 dark:bg-grey-700;
-    
-    @apply text-base;
-    @apply text-grey-800 dark:text-gray-200;
-    
-    @apply px-4 pt-2 pb-1;
-    @apply rounded;
-    @apply w-[450px];
-
-    @apply focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-green-300 dark:focus:ring-green-700;
-  }
-
-  button {
-    @apply bg-grey-300 dark:bg-grey-700 hover:bg-grey-100 hover:dark:bg-grey-900;
-
-    @apply border border-gray-500;
-
-    @apply text-sm font-medium;
-    @apply text-grey-800 dark:text-gray-200 text-base;
-    
-    @apply px-4 py-2;
-    @apply shadow-sm;
-    @apply rounded-md;
-
-    @apply focus:outline-none focus:ring-2 focus:ring-offset-0 focus:ring-green-700 dark:focus:ring-green-700;
-  }
-
-  dl {
-    @apply space-y-4;
-  }
-
-  dt {
-    @apply text-lg text-grey-600 dark:text-gray-400;
-    @apply pt-4;
-  }
-
-  dd {
-    @apply flex flex-col;
-    @apply text-xl;
-    @apply space-y-4;
   }
 
 </style>

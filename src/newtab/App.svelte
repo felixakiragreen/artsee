03cf2ca8e5de35054a51cffbc9845c5af49a2891
Felix Green <work@felix.green>
2021-04-25 00:28:11 -0400

    move init to App

--- src/newtab/App.svelte
+++ src/newtab/App.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
+  import { model } from './store'
+
   import Settings from './Settings.svelte'
   import Frame from './Frame.svelte'
+
+  const init = async () => {
+    await model.initialize()
+  }
+
+  onMount(init)
 
 </script>
 
@@ -15,7 +24,7 @@
   main {
     @apply bg-grey-100 dark:bg-grey-900;
     @apply w-screen h-screen max-h-screen max-w-full;
-    @apply flex items-center justify-center;    
+    @apply flex items-center justify-center;
   }
 
 </style>

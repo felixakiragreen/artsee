6d4f35df13caa986319f338205181beca3e1cab8
Felix Green <work@felix.green>
2021-04-25 20:41:31 -0400

    add types for storage chunking

--- src/model/types.ts
+++ src/model/types.ts
@@ -11,11 +11,27 @@ export type Model = {
   assets?: OpenSeaAsset[] // array of open sea assets
 }
 
-// nftAll
-// nft001
-// nft002
+export type StorageModel = {
+  wallet?: WalletAddress // wallet address
+  synced?: {
+    started?: boolean
+    count?: number
+    finished?: number // time the sync was finished
+    error?: string
+  }
+  ass000?: OpenSeaAsset[]
+  ass001?: OpenSeaAsset[]
+  ass002?: OpenSeaAsset[]
+  ass003?: OpenSeaAsset[]
+  ass004?: OpenSeaAsset[]
+  ass005?: OpenSeaAsset[]
+  ass006?: OpenSeaAsset[]
+  ass007?: OpenSeaAsset[]
+  ass008?: OpenSeaAsset[]
+  ass009?: OpenSeaAsset[]
+}
 
 export type OpenSeaAsset = {
-  address: string
-  tokenId: string
+  c: string
+  t: string
 }

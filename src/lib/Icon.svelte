0cef9402df601df11bd65915e6aa42189d320dd1
Felix Green <work@felix.green>
2021-04-25 21:08:51 -0400

    refactor common icon shit

---
+++ src/lib/Icon.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  // export let cls
+</script>
+
+<div>
+  <slot />
+</div>
+
+<style style lang="postcss">
+
+div {
+  @apply p-2;
+  @apply rounded;
+  @apply cursor-pointer;
+
+  @apply text-grey-900 dark:text-gray-100 hover:text-green-700 hover:dark:text-green-300;
+  @apply hover:bg-grey-300 hover:dark:bg-gray-700;
+  @apply bg-opacity-25 hover:bg-opacity-50; 
+}
+
+</style>

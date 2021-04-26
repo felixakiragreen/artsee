82ff214005f89b686d5cfb51ac52155394fe2566
Felix Green <work@felix.green>
2021-04-26 13:53:56 -0400

    replace Icons

---
+++ src/lib/IconButton.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  export let onClick
+</script>
+
+<div on:click={onClick}>
+  <slot />
+</div>
+
+<style style lang="postcss">
+
+div {
+  @apply p-2;
+  @apply rounded;
+  @apply cursor-pointer;
+
+  @apply text-grey-900 dark:text-gray-100;
+  @apply hover:text-green-700 hover:dark:text-green-300;
+
+  @apply bg-grey-200 dark:bg-grey-800;
+  @apply hover:bg-grey-300 hover:dark:bg-gray-700;
+
+  @apply bg-opacity-25 hover:bg-opacity-50;
+  @apply transition;
+
+  @apply h-10 w-10;
+}
+
+
+
+</style>

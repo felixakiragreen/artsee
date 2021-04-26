8b398880345f57c046ebe33c11bef5d3f0a73ac8
Felix Green <work@felix.green>
2021-04-26 13:54:40 -0400

    add default empty view

---
+++ src/newtab/Empty.svelte
@@ -0,0 +1,17 @@
+<div class="empty">
+  <p>🖼 👀</p>
+</div>
+
+<style style lang="postcss">
+
+.empty {
+  width: 50vw;
+  height: 50vh;
+  @apply flex items-center justify-center;
+}
+
+p {
+  @apply text-8xl;
+}
+
+</style>

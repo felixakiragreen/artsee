019ab886c7fa9626fd89f090c0fcc4c52ab96ed2
Felix Green <work@felix.green>
2021-04-25 20:42:38 -0400

    add storage chunking

--- src/model/storage.ts
+++ src/model/storage.ts
@@ -1,4 +1,6 @@
-import type { WalletAddress, Model, OpenSeaAsset } from './types'
+import chunk from 'lodash.chunk'
+
+import type { WalletAddress, Model, StorageModel, OpenSeaAsset } from './types'
 
 export const loadStorage = async (): Promise<Model> => {
   return new Promise((resolve, reject) => {
@@ -46,3 +48,76 @@ export const deleteStorage = async () => {
     console.log('storage.delStorage()')
   })
 }
+
+export const pad = (id: number | string, length = 3): string =>
+  `000${id}`.slice(-length)
+
+const CHUNK_SIZE = 100
+
+export const mapToStorage = (model: Model): StorageModel => {
+  const chunkedAssets = chunk(model.assets, CHUNK_SIZE)
+
+  console.log('b4', model)
+
+  let storage = { ...model }
+
+  delete storage.assets
+
+  console.log('a5', storage)
+
+  chunkedAssets.map((eachChunk, index) => {
+    storage[`ass${pad(index)}`] = [...eachChunk]
+  })
+
+  console.log('a6', storage)
+
+  return storage
+}
+
+export const mapFromStorage = (storage: StorageModel): Model => {
+  let assets: OpenSeaAsset[] = []
+
+  let model = {
+    ...(storage || {}),
+  }
+
+  console.log('b4', model)
+
+  for (let i = 0; i < 9; i++) {
+    let id = `ass${pad(i)}`
+    let chunkedAssets = storage[id]
+
+    console.log({ chunkedAssets })
+
+    if (chunkedAssets) {
+      assets = assets.concat(chunkedAssets)
+      delete model[id]
+    }
+  }
+
+  model.assets = assets
+
+  console.log('a5', model)
+
+  return model
+}
+
+// nftAll
+// nft000
+// nft001
+// nft002
+
+// update assets....
+
+/*
+on load....
+
+join all the assets into a massive array
+on save 
+
+
+chunk
+
+
+
+*/

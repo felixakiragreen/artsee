0d1c9f4307ec784b4b109cb6e548906d1441bab7
Felix Green <work@felix.green>
2021-04-26 16:02:29 -0400

    add timeouts for autohiding UI

--- src/newtab/App.svelte
+++ src/newtab/App.svelte
@@ -1,19 +1,62 @@
 <script lang="ts">
   import { onMount } from 'svelte'
-  import { model } from '../model'
+  import { model, ui } from '../model'
 
   import Settings from './Settings.svelte'
   import Frame from './Frame2.svelte'
 
   const init = async () => {
     await model.initialize()
+
+    timeoutUI()
   }
 
   onMount(init)
+  
+  $: ({ showAllControls } = ui)
+
+  let timerUI
+  let timerMM
+
+  const delayUI = 5000
+  const delayMM = 500
+  let throttled = false
+
+  const onMouseMove = () => {
+    if (!throttled) {
+      console.log("onMouseMove")
+      if (timerUI) {
+        clearTimeout(timerUI)
+      }
+      if (timerMM) {
+        clearTimeout(timerMM)
+      }
+      throttled = true
+  
+      if (!$showAllControls) {
+        showAllControls.set(true)
+      }
+      timeoutMM()
+      timeoutUI()
+    }
+  }
+
+  const timeoutUI = () => {
+    timerUI = setTimeout(() => {
+      console.log("timeoutUI()")
+      showAllControls.set(false)
+    }, delayUI)
+  }
+
+  const timeoutMM = () => {
+    timerMM = setTimeout(() => {
+      throttled = false
+    }, delayMM)
+  }
 
 </script>
 
-<main>
+<main on:mousemove={onMouseMove}>
 
   <Settings />
   <Frame />

b713af6fec6b1ce895296d6962f7158ebea378ab
Felix Green <work@felix.green>
2021-04-27 12:50:41 -0400

    build out dev component

---
+++ src/newtab/Dev.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { fade } from 'svelte/transition'
+  // import pkg from '../manifest.json'
+
+  import { ui } from '../model'
+
+  $: ({ showAllText } = ui)
+</script>
+
+
+{#if $showAllText}
+  <aside transition:fade={{ duration: 300 }}>
+
+    <p>artsee BETA v0.1</p>
+
+    <p>
+      join our <a href="https://discord.gg/6j65H7nqSd" target="_blank">discord</a><br />
+      to report bugs<br />
+      or request features!
+    </p>
+
+  </aside>
+{/if}
+
+<style style lang="postcss">
+
+  aside {
+    @apply absolute;
+    @apply top-4 right-4;
+    @apply font-mono text-right opacity-50;
+    @apply z-50;
+    @apply space-y-4;
+  }
+
+  a {
+    @apply underline hover:no-underline;
+  }
+
+</style>

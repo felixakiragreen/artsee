9dd3deab7214841ad00a644fc321071e7d1524e9
Felix Green <work@felix.green>
2021-04-27 12:50:32 -0400

    use the new artsee art

--- src/newtab/Empty.svelte
+++ src/newtab/Empty.svelte
@@ -1,5 +1,11 @@
+<script>
+  import ArtSee from '../lib/ArtSee.svelte'
+
+</script>
+
 <div class="empty">
-  <p>🖼 👀</p>
+  <!-- <p>🖼 👀</p> -->
+  <ArtSee />
 </div>
 
 <style style lang="postcss">
@@ -8,10 +14,13 @@
   width: 50vw;
   height: 50vh;
   @apply flex items-center justify-center;
+
+  @apply text-grey-900 opacity-50;
 }
 
 p {
   @apply text-8xl;
+  font-size: 256px;
 }
 
 </style>

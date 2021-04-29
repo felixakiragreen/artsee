9427bc5cfd19323686fbfd90d07f86d5ae7d11a5
Felix Green <work@felix.green>
2021-04-29 13:17:27 -0400

    add comments

--- src/model/ui.ts
+++ src/model/ui.ts
@@ -2,8 +2,33 @@ import { writable } from 'svelte/store'
 
 const showAllControls = writable(true)
 const showAllText = writable(true)
+// const autoHide = writable(true)
 
 export const ui = {
   showAllControls,
   showAllText,
 }
+
+/*
+
+settings
+
+backgrounds
+- light
+- dark
+- dominant color
+
+autohide
+- controls
+- text
+
+screensave
+
+
+TODO: move the timers to the store
+so that I can reset then on interacting with iframes
+
+
+
+
+*/

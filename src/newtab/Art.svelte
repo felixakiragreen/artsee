6344754064be037189e841b73b62266cfbf67598
Felix Green <work@felix.green>
2021-04-29 16:27:16 -0400

    refactor

--- src/newtab/Art.svelte
+++ src/newtab/Art.svelte
@@ -3,13 +3,20 @@
   import { fade } from 'svelte/transition'
 
   import { fetchOpenSeaAsset, ui } from '../model'
+  import { assets } from '../model/store2'
   import type { OpenSeaAsset } from '../model'
-  import { currentAssetIndex, currentAsset, cachedAsset } from '../model/cache'
+  import { viewingIndex, viewingAsset, cachedAssetData } from '../model/cache'
+
+  import {
+    getType,
+    getUrl,
+    getImgUrl
+  } from '../model/funcs'
 
   import Empty from './Empty.svelte'
 
-  export let index: number
-  export let assets: OpenSeaAsset[]
+  // export let index: number
+  // export let assets: OpenSeaAsset[]
   export let getBgColor: () => void
 
   // onMount(async () => {
@@ -28,143 +35,64 @@
   //   })
   // }
 
-  const getFileTypeFromUrl = (url: string): string => {
-    let type = url.split(".").pop()
-
-    // console.log('getFileTypeFromUrl()', url, type)
-
-    if (type && type.length < 5) {
-      return type
-    }
-  }
-
-  type NiftyTag = "video" | "img"
-  type Nifty = {
-    tag: NiftyTag
-    file: string
-  }
-
-  const getType = (asset): Nifty => {
-    let file: string
-    let tag: NiftyTag
   
-    // animation
-    if (asset["animation_url"]) {
-      file = getFileTypeFromUrl(asset["animation_url"])
-      tag = "video" 
-    }
-
-    if (!file && asset["animation_original_url"]) {
-      file = getFileTypeFromUrl(asset["animation_original_url"])
-      tag = "video"
-    }
-
-    // image
-    if (!file && asset["image_url"]) {
-      file = getFileTypeFromUrl(asset["image_url"])
-      tag = "img"
-    }
-
-    if (!file && asset["image_original_url"]) {
-      file = getFileTypeFromUrl(asset["image_original_url"])
-      tag = "img"
-    }
-
-    // console.log('getType()', tag, file)
-
-    return {
-      tag, file
-    }
-  }
-  
-  const getUrl = (asset) => {
-    if (asset["animation_url"]) {
-      return asset["animation_url"]
-    }
-    else if (asset["animation_original_url"]) {
-      return asset["animation_original_url"]
-    }
-    else if (asset["image_url"]) {
-      return asset["image_url"]
-    }
-    else if (asset["image_original_url"]) {
-      return asset["image_original_url"]
-    } 
-    else {
-      console.error('could not find URL for:', { asset })
-      return ''
-    }
-  }
-  
-  const getImgUrl = (asset) => {
-    if (asset["image_url"]) {
-      return asset["image_url"]
-    }
-    else if (asset["image_original_url"]) {
-      return asset["image_original_url"]
-    } 
-    else {
-      console.error('could not find URL for:', { asset })
-      return ''
-    }
-  }
 
   $: ({ showAllText } = ui)
 </script>
 
 
 
-{#if $currentAsset}
-{#await $cachedAsset}
+{#if $viewingAsset}
+  {#await $cachedAssetData}
 
-  <Empty />
+    <Empty />
 
-{:then nftData}
+  {:then nftData}
 
-  <div class="bg" transition:fade={{ duration: 600 }} />
+    <div class="bg" transition:fade={{ duration: 600 }} />
 
-  <section transition:fade={{ duration: 300 }}>
-    {#if $showAllText}
-      <div class="caption" transition:fade={{ duration: 300 }}><span class="index">{index}</span></div>
-    {/if}
-    <div class="frame">
-      {#if getType(nftData).tag === "video"}
-        {#if getType(nftData).file === "html"}
-          <iframe
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            frameborder="0"
-            sandbox="allow-scripts"
-            src={getUrl(nftData)}
-            width="100%"
-            height="100%"
-            style="min-height: inherit;"
-            title={nftData["name"]}
-          ></iframe>
-        {:else}
-          <video loop autoplay>
-            <source src={getUrl(nftData)} type="video/mp4" />
-            <track default kind="captions" />
-          </video>
+    <section transition:fade={{ duration: 300 }}>
+      <!-- {#if $showAllText}
+        <div class="caption" transition:fade={{ duration: 300 }}><span class="index">{index}</span></div>
+      {/if} -->
+      <div class="frame">
+        {#if getType(nftData).tag === "video"}
+          {#if getType(nftData).file === "html"}
+            <iframe
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              frameborder="0"
+              sandbox="allow-scripts"
+              src={getUrl(nftData)}
+              width="100%"
+              height="100%"
+              style="min-height: inherit;"
+              title={nftData["name"]}
+            ></iframe>
+          {:else}
+            <video loop autoplay>
+              <source src={getUrl(nftData)} type="video/mp4" />
+              <track default kind="captions" />
+            </video>
+          {/if}
         {/if}
-      {/if}
-      <img
-        id={`img-${index}`}
-        src={getImgUrl(nftData)}
-        alt={nftData["name"]}
-        class="{getType(nftData).tag === "video" && "hide"}"
-        on:load={getBgColor}
-      />
-    </div>
-    {#if $showAllText}
-      <div class="caption" transition:fade={{ duration: 300 }}>{nftData["name"]}</div>
-    {/if}
-  </section>
+        <img
+          id={`img-${$viewingIndex}`}
+          src={getImgUrl(nftData)}
+          alt={nftData["name"]}
+          class="{getType(nftData).tag === "video" && "hide"}"
+          on:load={getBgColor}
+        />
+      </div>
+      <!-- {#if $showAllText}
+        <div class="caption" transition:fade={{ duration: 300 }}>{nftData["name"]}</div>
+      {/if} -->
+    </section>
 
-{:catch err}
+  {:catch err}
 
-  <Empty />
+    <Empty />
 
-{/await}
+  {/await}
 {/if}
 
 

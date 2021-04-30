eafc16af76bc48457514010f1fabf5cc395c6fac
Felix Green <work@felix.green>
2021-04-30 12:23:05 -0400

    add page number to controls

--- src/newtab/Controls.svelte
+++ src/newtab/Controls.svelte
@@ -1,16 +1,50 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
   import { fly } from 'svelte/transition'
   
-  import Icon, { ChevronLeft, ChevronRight, ChevronDoubleLeft, ChevronDoubleRight, Refresh } from 'svelte-hero-icons'
+  import Icon, { ChevronLeft, ChevronRight, ChevronDoubleLeft, ChevronDoubleRight, Refresh, ArrowsExpand } from 'svelte-hero-icons'
   import IconButton from '../lib/IconButton.svelte'
   
   import { ui } from '../model'
+  import { assets } from '../model/store2'
+  import { viewingIndex } from '../model/cache'
   
   export let onRandom
   export let onPrev
   export let onNext
   export let onFirst
   export let onLast
+  export let onExpand
+
+  let index = $viewingIndex + 1
+
+  onMount(async () => {
+    // subcribe to changes so input updates when navigating around
+    const unsub = viewingIndex.subscribe((vI) => {
+      if (vI + 1 !== index) {
+        // console.log("setting", vI, index)
+        index = vI + 1
+      }
+    })
+  })
+
+  const onIndexChange = () => {
+    // console.log("onIndexChange()", index)
+    if (index === 0) {
+      viewingIndex.set(0)
+    } else if (index > 0 && index < $assets.length) {
+      viewingIndex.set(index - 1)
+    } else if (index === null) {
+      // console.log("test null")
+      viewingIndex.set(0)
+    } else if (index > $assets.length) {
+      // console.log("test over")
+      viewingIndex.set($assets.length - 1)
+    } else if (index < 0) {
+      // console.log("test neg")
+      viewingIndex.set(0)
+    }
+  }
 
   $: ({ showAllControls } = ui)
 </script>
@@ -47,6 +81,15 @@
         <Icon src="{ChevronLeft}" />
       </IconButton>
     </div>
+    <div class=pagination>
+      <input
+        type="number"
+        min=0 max={$assets.length - 1}
+        bind:value={index}
+        on:change={onIndexChange}
+      />
+      <div>/{$assets.length}</div>
+    </div>
     <div>
       <IconButton onClick={onNext}>
         <Icon src="{ChevronRight}" />
@@ -57,16 +100,62 @@
         <Icon src="{ChevronDoubleRight}" />
       </IconButton>
     </div>
+    <div>
+      <IconButton onClick={onExpand}>
+        <Icon src="{ArrowsExpand}" />
+      </IconButton>
+    </div>
   </div>
 {/if}
 
 <style style lang="postcss">
 
-.controls {
-  @apply flex flex-row justify-center;
-  @apply space-x-4;
-  @apply p-4;
-  @apply fixed bottom-0 left-0 right-0;
-}
+  .controls {
+    @apply flex flex-row justify-center;
+    @apply space-x-4;
+    @apply p-4;
+    @apply fixed bottom-0 left-0 right-0;
+  }
+
+  .pagination {
+    @apply text-lg;
+
+    @apply flex flex-row items-center;
+
+    > div {
+      @apply text-grey-900 dark:text-gray-100;
+      @apply bg-grey-200 dark:bg-grey-800;
+
+      @apply bg-opacity-25;
+      @apply backdrop-filter backdrop-blur-md;
+
+      @apply h-10 p-2;
+      @apply rounded rounded-l-none;
+    }
+  }
+  input[type="number"] {
+    @apply text-grey-900 dark:text-gray-100;
+    @apply hover:text-green-700 hover:dark:text-green-300;
+
+    @apply bg-grey-200 dark:bg-grey-800;
+    @apply hover:bg-green-300 hover:dark:bg-green-700;
+
+    @apply bg-opacity-25 hover:bg-opacity-50;
+    @apply backdrop-filter backdrop-blur-md;
+    @apply transition;
+
+    @apply h-10;
+    @apply p-2;
+    @apply rounded rounded-r-none;
+
+    @apply ring-green-300 dark:ring-green-700;
+    @apply focus:outline-none focus:ring-2 focus:ring-offset-0;
+  }
+
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
 
 </style>

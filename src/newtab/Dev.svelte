f35a47bb8bad76d9ea4f4aa8535f0ea9190a647e
Felix Green <work@felix.green>
2022-01-16 12:05:02 -0500

    bump version

--- src/newtab/Dev.svelte
+++ src/newtab/Dev.svelte
@@ -1,41 +1,38 @@
 <script lang="ts">
-  import { fade } from 'svelte/transition'
-  // import pkg from '../manifest.json'
+	import { fade } from 'svelte/transition'
+	// import pkg from '../manifest.json'
 
-  import { ui } from '../model'
+	import { ui } from '../model'
 
-  $: ({ showAllText } = ui)
+	$: ({ showAllText } = ui)
 </script>
 
-
 {#if $showAllText}
-  <aside transition:fade={{ duration: 300 }}>
+	<aside transition:fade={{ duration: 300 }}>
+		<p>artsee BETA v0.3.0</p>
 
-    <p>artsee BETA v0.2.0</p>
-
-    <p>
-      join our <a href="https://discord.gg/6j65H7nqSd" target="_blank">discord</a><br />
-      to report bugs<br />
-      or request features
-    </p>
-
-  </aside>
+		<p>
+			join our <a href="https://discord.gg/6j65H7nqSd" target="_blank"
+				>discord</a
+			><br />
+			to report bugs<br />
+			or request features
+		</p>
+	</aside>
 {/if}
 
 <style style lang="postcss">
+	aside {
+		@apply absolute;
+		@apply top-4 right-4;
+		@apply font-mono text-right;
+		@apply z-50;
+		@apply space-y-4;
 
-  aside {
-    @apply absolute;
-    @apply top-4 right-4;
-    @apply font-mono text-right;
-    @apply z-50;
-    @apply space-y-4;
+		@apply text-grey-500;
+	}
 
-    @apply text-grey-500;
-  }
-
-  a {
-    @apply underline hover:no-underline;
-  }
-
+	a {
+		@apply underline hover:no-underline;
+	}
 </style>

c6ad2c2fba02140cc25ef0878ce341c6fdb5a998
Felix Green <work@felix.green>
2022-01-16 12:06:00 -0500

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,26 @@
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import svelte from '@sveltejs/vite-plugin-svelte'
 import { chromeExtension } from 'vite-plugin-chrome-extension'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), chromeExtension()],
-  resolve: {
-    alias: [{ find: '@', replacement: '/src' }],
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        manifest: 'src/manifest.json',
-        // newtab: 'src/newtab/index.html',
-      },
-      output: {
-        name: 'ArtSee',
-      },
-    },
-    outDir: 'builds/artsee',
-  },
-  optimizeDeps: {
-    include: ['svelte-hero-icons'],
-  },
+	plugins: [svelte(), chromeExtension()],
+	resolve: {
+		alias: [{ find: '@', replacement: '/src' }],
+	},
+	build: {
+		rollupOptions: {
+			input: {
+				manifest: 'src/manifest.json',
+				// newtab: 'src/newtab/index.html',
+			},
+			output: {
+				name: 'ArtSee',
+			},
+		},
+		outDir: 'builds/artsee',
+	},
+	optimizeDeps: {
+		include: ['svelte-hero-icons'],
+	},
 })
